1e572e9627736375f0ed11d975dbba361802b220
gigihprasetia <82494987+gigihprasetia@users.noreply.github.com>
2023-01-03 11:13:41 +0700

    swiper

--- components/Swiper.js
+++ components/Swiper.js
@@ -1,6 +1,14 @@
 import React, {useState} from 'react';
-import {View, SafeAreaView, Image, TouchableOpacity} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  Text,
+  Dimensions,
+} from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import {SwiperFlatList} from 'react-native-swiper-flatlist';
 
 const Swipercompo = () => {
   const [indexImages, setIndexImages] = useState(0);
@@ -26,53 +34,75 @@ const Swipercompo = () => {
   };
 
   return (
-    <SafeAreaView>
-      <View
-        style={{
-          width: '100%',
-          height: 200,
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          paddingHorizontal: 10,
-        }}>
-        {/* <Text>prev</Text> */}
-        <TouchableOpacity
-          onPress={() => prevImages()}
-          style={{
-            position: 'absolute',
-            color: 'white',
-            zIndex: 10,
-            left: 20,
-          }}>
-          <AntDesign size={25} color={'white'} name="leftcircle" />
-        </TouchableOpacity>
-        {/* <View
-          style={{
-            width: 100,
-            height: 100,
-            backgroundColor: 'red',
-            position: 'absolute',
-            zIndex: 20,
-          }}></View> */}
-        <Image
-          style={{width: '100%', height: '100%', borderRadius: 20}}
-          source={{
-            uri: images[indexImages],
-          }}
-        />
-        <TouchableOpacity
-          onPress={() => nextImages()}
-          style={{
-            position: 'absolute',
-            color: 'white',
-            zIndex: 10,
-            right: 20,
-          }}>
-          <AntDesign size={25} color={'white'} name="rightcircle" />
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+    // <SafeAreaView>
+    //   <View
+    //     style={{
+    //       width: '100%',
+    //       height: 200,
+    //       display: 'flex',
+    //       flexDirection: 'row',
+    //       alignItems: 'center',
+    //       paddingHorizontal: 10,
+    //     }}>
+
+    //     <TouchableOpacity
+    //       onPress={() => prevImages()}
+    //       style={{
+    //         position: 'absolute',
+    //         color: 'white',
+    //         zIndex: 10,
+    //         left: 20,
+    //       }}>
+    //       <AntDesign size={25} color={'white'} name="leftcircle" />
+    //     </TouchableOpacity>
+
+    //     <Image
+    //       style={{width: '100%', height: '100%', borderRadius: 20}}
+    //       source={{
+    //         uri: images[indexImages],
+    //       }}
+    //     />
+    //     <TouchableOpacity
+    //       onPress={() => nextImages()}
+    //       style={{
+    //         position: 'absolute',
+    //         color: 'white',
+    //         zIndex: 10,
+    //         right: 20,
+    //       }}>
+    //       <AntDesign size={25} color={'white'} name="rightcircle" />
+    //     </TouchableOpacity>
+    //   </View>
+
+    // </SafeAreaView>
+    <View>
+      <SwiperFlatList
+        autoplay
+        autoplayDelay={2}
+        autoplayLoop
+        index={1}
+        showPagination
+        data={images}
+        renderItem={({item, index}) => {
+          return (
+            <View
+              style={{
+                width: Dimensions.get('window').width,
+                paddingHorizontal: Dimensions.get('window').width * 0.05,
+              }}>
+              <Image
+                style={{
+                  // width: Dimensions.get('window').width,
+                  height: 200,
+                  borderRadius: 20,
+                }}
+                source={{uri: item}}
+              />
+            </View>
+          );
+        }}
+      />
+    </View>
   );
 };
 
